f113b0f1e7a966419fb6d3f659960fb941a76d72
norimyxxxo <32965473+norimyxxxo@users.noreply.github.com>
2017-10-25 22:47:30 +0600

    Add files via upload

--- judge_unit.c
+++ judge_unit.c
@@ -2,12 +2,34 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <wait.h>
+#include <fcntl.h>
 
 int
 main (void)
 {
-    int fd[2];
+    int prog_checker[2];
+    int fd, i;
     int pid, pid1, pid2;
+    char * test_number_dat = NULL;
+    char * test_number_ans = NULL;
+    char * problem = NULL;
+    problem = (char*)malloc(sizeof(char) * 4);
+    test_number_dat = (char*)malloc(sizeof(char) * 7);
+    test_number_dat[0] = '0';
+    test_number_dat[1] = '0';
+    test_number_dat[2] = '0';
+    test_number_dat[3] = '.';
+    test_number_dat[4] = 'd';
+    test_number_dat[5] = 'a';
+    test_number_dat[6] = 't';
+    test_number_ans = (char*)malloc(sizeof(char) * 7);
+    test_number_ans[0] = '0';
+    test_number_ans[1] = '0';
+    test_number_ans[2] = '0';
+    test_number_ans[3] = '.';
+    test_number_ans[4] = 'a';
+    test_number_ans[5] = 'n';
+    test_number_ans[6] = 's';
     pid = fork();
     if (pid == 0)
     {
@@ -17,28 +39,37 @@ main (void)
     {
         wait(NULL);
     }
-    pipe(fd);
-    pid1 = fork();
-    if (pid1 == 0)
+    for(i = 1; i <= 2; i++)
     {
-        close(fd[0]);
-        dup2(fd[1], 1);
-        close(fd[1]);
-        execlp("./01", "./01", NULL);
-        _exit(1);
+        pipe(prog_checker);
+        pid1 = fork();
+        if (pid1 == 0)
+        {
+            test_number_dat[2] = i + '0';
+            fd = open(test_number_dat, O_CREAT);
+            close(prog_checker[0]);
+            dup2(fd, 0);
+            close(fd);
+            dup2(prog_checker[1], 1);
+            close(prog_checker[1]);
+            execlp("./01", "./01", NULL);
+            return 0;
+            exit(1);
+        }
+        pid2 = fork();
+        if (pid2 == 0)
+        {
+            close(prog_checker[1]);
+            dup2(prog_checker[0], 0);
+            close(prog_checker[0]);
+            test_number_ans[2] = i +'0';
+            execlp("./checker", "./checker", test_number_ans, NULL);
+            exit(1);
+        }
+        close(prog_checker[0]);
+        close(prog_checker[1]);
+        wait(NULL);
+        wait(NULL);
     }
-    pid2 = fork();
-    if (pid2 == 0)
-    {
-        close(fd[1]);
-        dup2(fd[0], 0);
-        close(fd[0]);
-        execlp("./checker", "./checker", "fans.txt", NULL);
-        _exit(1);
-    }
-    close(fd[0]);
-    close(fd[1]);
-    wait(NULL);
-    wait(NULL);
     return 0;
 }
