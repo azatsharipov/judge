568a8cea7b675ba8e04d0bb98d59be0c4e3ad450
norimyxxxo <kabi888u@gmail.com>
2017-10-26 11:19:20 +0300

    tmp_judge_unit

--- judge_unit.c
+++ judge_unit.c
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 
 int
-main (void)
+main (int argc, char **argv)
 {
     int prog_checker[2];
     int fd, i;
@@ -15,6 +15,7 @@ main (void)
     char * problem = NULL;
     problem = (char*)malloc(sizeof(char) * 4);
     test_number_dat = (char*)malloc(sizeof(char) * 7);
+    test_number_ans = (char*)malloc(sizeof(char) * 7);
     test_number_dat[0] = '0';
     test_number_dat[1] = '0';
     test_number_dat[2] = '0';
@@ -22,7 +23,6 @@ main (void)
     test_number_dat[4] = 'd';
     test_number_dat[5] = 'a';
     test_number_dat[6] = 't';
-    test_number_ans = (char*)malloc(sizeof(char) * 7);
     test_number_ans[0] = '0';
     test_number_ans[1] = '0';
     test_number_ans[2] = '0';
@@ -30,15 +30,20 @@ main (void)
     test_number_ans[4] = 'a';
     test_number_ans[5] = 'n';
     test_number_ans[6] = 's';
+
+
     pid = fork();
     if (pid == 0)
     {
-        execlp("gcc", "gcc", "-Wall", "01.c", "-o", "01", NULL);
+        execlp("gcc", "gcc", "-Wall", argv[1], "-o", "prog", NULL);
     }
     else
     {
         wait(NULL);
     }
+
+
+
     for(i = 1; i <= 2; i++)
     {
         pipe(prog_checker);
@@ -52,7 +57,7 @@ main (void)
             close(fd);
             dup2(prog_checker[1], 1);
             close(prog_checker[1]);
-            execlp("./01", "./01", NULL);
+            execlp("./prog", "./prog", NULL);
             return 0;
             exit(1);
         }
@@ -71,5 +76,6 @@ main (void)
         wait(NULL);
         wait(NULL);
     }
+
     return 0;
 }
